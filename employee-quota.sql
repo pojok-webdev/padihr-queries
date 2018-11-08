b8aaebd975f2aebe0817b67fdbb637571bd419e4
pujie <puji@gmail.com>
2018-11-08 16:56:21 +0700

    reorganize text

--- employee-quota.sql
+++ employee-quota.sql
@@ -3,8 +3,15 @@ A.fullname,
 timestampdiff(YEAR,joindate,now()) workingduration,
 B.level,
 C.prev_level,
-case when (timestampdiff(YEAR,joindate,now())=0) then '0' when (timestampdiff(YEAR,joindate,now())=1) then 'prorate' when (timestampdiff(YEAR,joindate,now())>1) then B.quota else 'undefined' end curleavequota  
-from(select a.fullname,b.id_employee,min(b.tgl_mulai)joindate from employee a left outer join employment b on b.id_employee=a.id_employee group by a.fullname,b.id_employee) A
+case 
+when (timestampdiff(YEAR,joindate,now())=0) then '0' 
+when (timestampdiff(YEAR,joindate,now())=1) then 'prorate' 
+when (timestampdiff(YEAR,joindate,now())>1) then B.quota 
+else 'undefined' end curleavequota  
+from(
+select a.fullname,b.id_employee,min(b.tgl_mulai)joindate from employee a 
+left outer join employment b on b.id_employee=a.id_employee 
+group by a.fullname,b.id_employee) A
 left outer join (select id_employee,max(id_level) level, case max(id_level) 
 when 1 then 12 
 when 2 then 12
@@ -18,7 +25,7 @@ when 2 then 12
 when 3 then 15 
 when 4 then 18
 when 5 then 24 
-end quota
- from employment where status='0' group by id_employee) C on C.id_employee=A.id_employee
+end quota 
+from employment where status='0' group by id_employee) C on C.id_employee=A.id_employee
 ;
 
